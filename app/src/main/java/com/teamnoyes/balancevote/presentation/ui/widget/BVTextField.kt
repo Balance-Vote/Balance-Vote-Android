9fab03c55df0f9186b93eefb0643a35df0ad939d
CrewDaniel <alaskay@naver.com>
2022-02-22 19:45:43 +0900

    feat: clear text after send

--- app/src/main/java/com/teamnoyes/balancevote/presentation/ui/widget/BVTextField.kt
+++ app/src/main/java/com/teamnoyes/balancevote/presentation/ui/widget/BVTextField.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -12,7 +13,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment.Companion.CenterStart
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
@@ -20,7 +24,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.teamnoyes.balancevote.ui.theme.BalanceVoteTheme
-
 
 /**
  * 버튼은 오른쪽 정렬, 남는 공간을 TextField로 채운다.
@@ -37,29 +40,59 @@ fun BVInput(
     onSendButtonClick: (String) -> Unit
 ) {
     var textState by remember { mutableStateOf(TextFieldValue()) }
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(64.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Box(
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+
+    Column() {
+        Row(
             modifier = Modifier
-                .weight(1f)
+                .fillMaxWidth()
+                .height(64.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            BVTextField(textState) { textState = it }
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                BVTextField(
+                    textFieldValue = textState,
+                    onTextChanged = { textState = it },
+                    onSendButtonClick = {
+                        onSendButtonClick(it.text)
+                        textState = TextFieldValue("")
+                        focusManager.clearFocus()
+                    },
+                    focus = focusRequester
+                )
+            }
+            if (enableButton) {
+                Spacer(modifier = Modifier.width(16.dp))
+                BVButton(textState.text.isEmpty()) {
+                    onSendButtonClick(textState.text)
+                    textState = TextFieldValue("")
+                    focusManager.clearFocus()
+                }
+            }
         }
-        if (enableButton) {
+        Spacer(modifier = Modifier.height(4.dp))
+        Row() {
             Spacer(modifier = Modifier.width(16.dp))
-            BVButton(textState.text.isEmpty()) { onSendButtonClick(textState.text) }
+            Text(text = "문자열 리소스로 출력 예정")
         }
     }
 }
 
 @Composable
+fun BVAlertText() {
+
+}
+
+@Composable
 fun BVTextField(
     textFieldValue: TextFieldValue,
-    onTextChanged: (TextFieldValue) -> Unit
+    onTextChanged: (TextFieldValue) -> Unit,
+    onSendButtonClick: (TextFieldValue) -> Unit,
+    focus: FocusRequester
 ) {
 //    text 저장 위치를 어디에?
     Box(
@@ -81,10 +114,14 @@ fun BVTextField(
                     .wrapContentHeight()
                     .fillMaxWidth()
                     .align(CenterStart)
-                    .padding(16.dp),
+                    .padding(16.dp)
+                    .focusRequester(focus),
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Text,
                     imeAction = ImeAction.Send
+                ),
+                keyboardActions = KeyboardActions(
+                    onSend = { onSendButtonClick(textFieldValue) }
                 )
             )
             if (textFieldValue.text.isEmpty()) {
@@ -123,7 +160,7 @@ fun PreviewTextFieldWithButton() {
             modifier = Modifier.fillMaxWidth(),
             color = MaterialTheme.colors.background
         ) {
-            BVInput(true, {})
+            BVInput(true) {}
         }
     }
 }
@@ -136,7 +173,7 @@ fun PreviewTextFieldWithoutButton() {
             modifier = Modifier.fillMaxWidth(),
             color = MaterialTheme.colors.background
         ) {
-            BVInput(false, {})
+            BVInput(false) {}
         }
     }
 }
